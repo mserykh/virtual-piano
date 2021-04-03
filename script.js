c0fe365aea3dc97bfae67468f8012ec9f8f4af04
Marie Serykh <marie.serykh@gmail.com>
2021-04-03 20:33:37 +0200

    feat: implement playing on clicks

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 const pianoElement = document.querySelector('.js-piano');
 
 pianoElement.addEventListener('click', onPianoClick);
-
+pianoElement.addEventListener('mouseover', removeTransition);
 window.addEventListener('keydown', onWindowKeyDown);
 
 function onWindowKeyDown(e) {
-    
-    highlightKey(e.key);
-    playSound(e.key);
+    const element = document.querySelector(`.js-piano-key[data-letter="${e.key}"]`);
+    if (!element) return;
+
+    const note = element.dataset.note;
+    const src = `assets/audio/${note}.mp3`;
+
+    highlightKey(element);
+    playSound(src);
 }
 
 function onPianoClick(e) {
     if (!e.target.classList.contains('js-piano-key')) return;
-
-    highlightKey();
-    playSound();
+    const note = e.target.dataset.note;
+    const src = `assets/audio/${note}.mp3`;
+    highlightKey(e.target);
+    playSound(src);
 }
 
-function playSound(key) {
-    const element = document.querySelector(`.js-piano-key[data-letter="${key}"]`);
-    if (!element) return;
-
-    console.log(element);
-
-    const note = element.dataset.note;
-    const src = `assets/audio/${note}.mp3`
-    console.log(src);
+function playSound(src) {
     const audio = new Audio();
     audio.src = src;
     audio.currentTime = 0;
     audio.play();
 }
 
-function highlightKey() {
+function highlightKey(element) {
+    if (element.classList.contains('piano-key--active')) {
+        element.classList.remove('piano-key--active');
+    }
+    element.classList.add('piano-key--active');
+}
 
+function removeTransition(e) {
+    console.log(e);
+    // classList.remove('piano-key--active');
 }
